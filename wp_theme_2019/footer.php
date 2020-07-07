65c74568967d3aa4fbcfd1698d796602603cf06b
flyhawk611@gmail.com <flyhawk611@gmail.com>
2020-07-07 12:27:42 +0900

    栗林さんHTML_Wordpress化_Lesson0

--- wp_theme_2019/footer.php
+++
@@ -1,8 +0,0 @@
-<footer id="footer" class="footer">
-  <div class="footer-inner">
-
-  </div><!--end footer-inner-->
-</footer>
-<?php wp_footer(); ?><!--システム・プラグイン用-->
-</body>
-</html>
--- wp_theme_2019/single.php
+++
@@ -1,8 +0,0 @@
-<?php get_header(); ?>
-<div class="container">
-  <div class="contents">
-
-  </div><!--end contents-->
-  <?php get_sidebar(); ?>
-</div><!--end container-->
-<?php get_footer(); ?>
